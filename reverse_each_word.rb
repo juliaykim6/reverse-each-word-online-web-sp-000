d9515f182e1179ba188b5254c670a2a1983fa640
Julia Kim <juliaykim6@gmail.com>
2021-01-16 21:30:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,3 @@
-def reverse_each_word (sentence)
-  sentence.split.collect do |word| word.reverse.join(" ")
-  end
+def reverse_each_word(snetence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
